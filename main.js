19909f4e0bb2813bd510ab9384e3f964a25d9fea
YelbisArts <99764343+YelbisArts@users.noreply.github.com>
2022-03-27 15:24:17 -0400

    Create main.js
    Codewars problems -- JavaScript

---
+++ main.js
@@ -0,0 +1,22 @@
+// Prompt 1
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Solution
+
+var stringToNumber = function(str){
+  mayo = parseInt(str)
+
+  return mayo;
+}
+
+console.log(stringToNumber)
+
+// Sample Tests
+describe( "stringToNumber", function(){
+  it( "should work for the examples" , function(){
+    Test.assertEquals(stringToNumber("1234"),1234)
+    Test.assertEquals(stringToNumber("605"), 605)
+    Test.assertEquals(stringToNumber("1405"),1405)
+    Test.assertEquals(stringToNumber("-7"),  -7)
+  });
+});
